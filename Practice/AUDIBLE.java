be8395e2b4f0b0de532a74e69029641f9d4c4290
Vikas Verma <111645400+vikas8852@users.noreply.github.com>
2023-10-23 23:31:48 +0530

    Day2

---
+++ Practice/AUDIBLE.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int tc=sc.nextInt();
+		while(tc-->0){
+	
+		    int x=sc.nextInt();
+		    if(x>=67 && x<=45000)
+		    System.out.println("yes");
+		    else
+		    System.out.println("no");
+		}
+	}
+}
