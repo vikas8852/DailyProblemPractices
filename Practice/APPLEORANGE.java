fb8e1ec48e331bc15359d340db59bf0dea186bfc
Vikas Verma <111645400+vikas8852@users.noreply.github.com>
2023-11-04 20:55:29 +0530

    Day 12

---
+++ Practice/APPLEORANGE.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int tc=sc.nextInt();
+		while(tc-->0){
+		    int app=sc.nextInt();
+		    int mang=sc.nextInt();
+		    int max=Math.min(app,mang);
+		   
+		     if(app==1 || mang==1)
+		        System.out.println("1");
+		     else{
+		        while(app!=mang){
+		            if(app>mang){
+		                app-=mang;
+		            }
+		            else if(mang>app){
+		                mang-=app;
+		            }
+		        }
+		        System.out.println(app);
+		       
+		    }
+		}
+	}
+}
