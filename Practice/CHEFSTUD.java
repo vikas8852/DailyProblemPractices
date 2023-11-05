781c16609aca3194ae604bbbe5d74bcadcefecaa
Vikas Verma <111645400+vikas8852@users.noreply.github.com>
2023-11-05 13:40:51 +0530

    Day 13

---
+++ Practice/CHEFSTUD.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int tc=sc.nextInt();
+		while(tc-->0){
+		    String s=sc.next();
+		    int count=0;
+		    for(int i=0;i<s.length()-1;i++){
+		       if (s.charAt(i) == '<' && s.charAt(i+1) == '>') {
+					count++;
+				}
+		    }
+		    System.out.println(count);
+		   
+		}
+	}
+}
