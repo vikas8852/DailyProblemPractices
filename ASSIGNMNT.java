a31549d43b309f2055b9e1a26bdf71c2f92e10fd
Vikas Verma <111645400+vikas8852@users.noreply.github.com>
2023-11-08 22:10:55 +0530

    Day1

---
+++ ASSIGNMNT.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+			Scanner sc=new Scanner(System.in);
+		int tc=sc.nextInt();
+		while(tc-->0){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int z=sc.nextInt();
+		    if(x*y<=z*24*60)
+		    System.out.println("YES");
+		    else
+		     System.out.println("NO");
+		}
+
+	}
+}
