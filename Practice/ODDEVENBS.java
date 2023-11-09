4a0f69d5620b57b02d25d5daa401431c08b074e4
Vikas Verma <111645400+vikas8852@users.noreply.github.com>
2023-11-09 20:33:55 +0530

    Day17

---
+++ Practice/ODDEVENBS.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int tc=sc.nextInt();
+		while(tc-->0){
+		    int n = sc.nextInt();
+		    int[] arr = new int[n];
+		     int count = 0;
+		    for(int i = 0; i < n; i++){
+		        arr[i] = sc.nextInt();
+		        if(arr[i] == 0){
+		            count++;
+		        }
+		    }
+		    
+		    if(count%2 == 0){
+		        System.out.println("Yes");
+		    }
+		    else{
+		        System.out.println("No");
+		    }
+		}
+
+	}
+}
