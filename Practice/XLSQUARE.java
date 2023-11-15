c89ce6c7e28c595cf774e4eb03647bb50a86d714
Vikas Verma <111645400+vikas8852@users.noreply.github.com>
2023-11-15 23:03:36 +0530

    Day23

---
+++ Practice/XLSQUARE.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int tc=sc.nextInt();
+		while(tc-->0){
+		    int n=sc.nextInt();
+		    int a=sc.nextInt();
+		  int sq=(int)Math.sqrt(n);
+		     System.out.println(sq*a);
+		}
+	}
+}
