64c8687e206c0ebf3e3739d4e49c974aac190d58
Vikas Verma <111645400+vikas8852@users.noreply.github.com>
2023-11-29 22:02:38 +0530

    DAY4

---
+++ Contest/SPCP1.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+	
+		    int w=sc.nextInt();
+		    int h=sc.nextInt();
+		    int cw=60;
+		    int ch=130;
+		    if(cw<=w && ch>=h)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		
+
+	}
+}
