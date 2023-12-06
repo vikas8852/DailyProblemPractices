93fbf0fee2ebfe4f7adbf1f71f646fca3cded145
Vikas Verma <111645400+vikas8852@users.noreply.github.com>
2023-12-06 22:19:37 +0530

    DAY43

---
+++ Practice/PASSTHEEXAM.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+		int tc=sc.nextInt();
+		while(tc-->0){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    if(a+b+c>=100 && a>=10 && b>=10 && c>=10)
+		    System.out.println("Pass");
+		   else
+		     System.out.println("Fail");
+		}
+	}
+}
